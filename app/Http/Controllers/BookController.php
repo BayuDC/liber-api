b313549f012b16665552943149b507b37099739f
BayuDC <dev.bayudc@gmail.com>
2022-03-16 10:25:14 +0700

    Handle GET requests

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\Book;
 
 class BookController extends Controller {
     public function index() {
+        return Book::all(['id', 'title', 'author']);
     }
-    public function show() {
+    public function show(Book $book) {
+        return $book->get(['id', 'title', 'author']);
     }
     public function store() {
     }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\Genre;
 
 class GenreController extends Controller {
     public function index() {
+        return Genre::all();
     }
-    public function show() {
+    public function show(Genre $genre) {
+        return $genre;
     }
     public function store() {
     }
