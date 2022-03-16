feb682ad419406abc6bec48df34db867a05adf8e
BayuDC <dev.bayudc@gmail.com>
2022-03-16 10:03:38 +0700

    Add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,16 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Book;
+use App\Models\Genre;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        Genre::create(['name' => 'Romance']);
+        Genre::create(['name' => 'Horror']);
+        Genre::create(['name' => 'Fantasy']);
+
+        Book::create(['title' => 'Buku 1', 'author' => 'Saya Sendiri', 'genre_id' => 1]);
+        Book::create(['title' => 'Buku 2', 'author' => 'Saya Sendiri', 'genre_id' => 1]);
+        Book::create(['title' => 'Buku 3', 'author' => 'Temen Saya', 'genre_id' => 2]);
+        Book::create(['title' => 'Ga Ada Judulnya', 'author' => 'Ga Tau Siapa', 'genre_id' => 3]);
     }
 }
